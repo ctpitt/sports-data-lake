a325000e5e32fb368835e05288abc802647f6e4d
christ <ctate93@outlook.com>
2025-05-24 00:42:47 -0400

    Fix CodePipeline policy attachment

--- main.tf
+++ main.tf
@@ -170,6 +170,11 @@ resource "aws_iam_role_policy_attachment" "codepipeline_s3_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "codepipeline_codestar_access" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeStarFullAccess"
+}
+
 resource "aws_codestarconnections_connection" "github_connection" {
   name          = "sports-data-github-connection"
   provider_type = "GitHub"
